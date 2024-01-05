7cb728d6c62cce9f733450fe18be6a491c518911
Mark Hegedus <Mark.Hegedus3@tesco.com>
2024-01-05 13:09:17 +0100

    Clean up

--- react_hacker_news/src/components/utils/convertPageInfo.ts
+++ react_hacker_news/src/components/utils/convertPageInfo.ts
@@ -1,6 +1,4 @@
 const convertPageInfo = (pageInfo: [number, number]) => {
-  // let pageCount = pageInfo[0];
-  // let end = pageInfo[1];
   let [pageCount, perPageContent] = pageInfo;
   return [0 + pageCount * perPageContent, perPageContent * (pageCount + 1)];
 };
--- react_hacker_news/src/pages/home.tsx
+++ react_hacker_news/src/pages/home.tsx
@@ -1,12 +1,4 @@
-import Launch from "@mui/icons-material/Launch";
-import {
-  CircularProgress,
-  IconButton,
-  Link,
-  List,
-  ListItem,
-  ListItemText,
-} from "@mui/material";
+import { List } from "@mui/material";
 import { useEffect, useState } from "react";
 import getNewest from "../api/getNewest";
 import getPosts, { HackerNewsStory } from "../api/getPosts";
@@ -39,14 +31,11 @@ const Home = () => {
 
     getPosts(myIDS.slice(loadBuffer)).then((fetchedPosts) => {
       setPosts(fetchedPosts);
-      // appendToPosts(fetchedPosts);
     });
   }, [myIDS]);
 
   useEffect(() => {
     if (posts.length > 0 && posts.length + bufferPosts.length <= myIDS.length) {
-      // const localPosts = posts;
-      // const localRenderedPosts = renderedPosts;
       setPosts([...bufferPosts, ...posts]);
     }
 
