a63fe7730f28991966d41bc845341f19635a2ca1
Mark Hegedus <Mark.Hegedus3@tesco.com>
2024-01-23 09:06:50 +0100

    React router

--- react_hacker_news/src/App.tsx
+++ react_hacker_news/src/App.tsx
@@ -1,16 +1,21 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Home from "./pages/home";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h2>HackerNews</h2>
-        <Home />
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>HackerNews</h2>
+          <Routes>
+            <Route path="" element={<Home />} />
+          </Routes>
+        </header>
+      </div>
+    </BrowserRouter>
   );
 };
 
